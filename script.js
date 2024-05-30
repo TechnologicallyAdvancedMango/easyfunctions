77ac08aa413c8f69a9004cf941b6f5d24598033c
techmango <kevinesko@hotmail.com>
2024-05-30 18:32:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Add Elements
+function addElement(tagName, content, attributes) {
+    // Create a new element
+    var element = document.createElement(tagName);
+
+    // Set the content
+    element.innerHTML = content;
+
+    // Set the attributes
+    if(attributes && typeof attributes === 'object') {
+        for(var key in attributes) {
+            if(attributes.hasOwnProperty(key)) {
+                element.setAttribute(key, attributes[key]);
+            }
+        }
+    }
+
+    // Append the element to the body
+    document.body.appendChild(element);
+}
